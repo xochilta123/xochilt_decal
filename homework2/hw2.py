5f1c4f4eaa20f6d166c434b0a6a0601a827b3fef
xochilta123 <xochilt0417@berkeley.edu>
2025-02-17 00:48:17 -0800

    Adding hw2 folder

---
+++ homework2/hw2.py
@@ -0,0 +1,10 @@
+# 1) Suppose your current working directory is ~/Desktop/classes/python_decal/. What command would allow you to navigate directly to ~/Desktop?
+# 2) What does ~/ mean?
+# 3) What's the difference between and abolute and a relative path in your terminal?
+# 4) What command would bring you back to your home directory?
+# 5) If you called rm ./ in your current working directory, what would it do? 
+# 6) In your current working directory, what does calling "git add" do? What about "git commit"? What about "git push"?
+# 7) Let's say you call "git add" in your current working directory and then "git status". What message would appear? What is it telling you?
+# 8) What has been the most frustrating error or bug you've encountered in this class so far? How did you troubleshoot and resolve it?
+# 9) How does cloning a repository differ from pulling from a repository?
+# 10) Tell me a fun fact!
