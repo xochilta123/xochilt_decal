c8748d142d4ff25e02a90f9036f25407b47d967c
xochilta123 <xochilt0417@berkeley.edu>
2025-02-17 22:39:17 -0800

    Adding hw1

---
+++ hw1.py
@@ -0,0 +1,11 @@
+# pwd - print working directory - use it to tell you where you are
+# cd - it means change directories - you would use this when you want to switch folders - you can use .. to go up
+# ls - it means list - use when you want to see all the files in a dircectory
+# mkdir - it makes make directory - use when you want to make a new directory 
+# cp - copy - use it to copy a folder/file - you can add -r if you are copying a directory to make sure everything in the directory is copied
+# mv - means move - use it to move files in a repository 
+# git lets you save your progress, github lets you push your work online, gitbash can open the terminal on windows (fancy GUI) 
+# windows powershell is the default UGI for windows and gitbash is an application you can install 
+# anaconda is a software that contains a lot of packages - conda is a package manager - python is a coding langauge 
+# jupityr notebook is an online notebook to store and run code - Jyputer lab is like jupyter notebook but with more features
+# VS Code is a code editor
